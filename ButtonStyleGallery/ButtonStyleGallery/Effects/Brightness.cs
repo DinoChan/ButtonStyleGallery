18968e6f085b1abc241a5beaea94239db186b49e
Dino.Chan <dino.chan@live.com>
2017-06-19 10:52:35 +0800

    add some pages

---
+++ ButtonStyleGallery/ButtonStyleGallery/Effects/Brightness.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation.Metadata;
+using Windows.UI.Composition;
+using Microsoft.Graphics.Canvas.Effects;
+using Microsoft.Toolkit.Uwp.UI.Animations.Effects;
+
+namespace ButtonStyleGallery.Effects
+{
+   
+
+    public class Brightness : AnimationEffect
+    {
+        /// <summary>
+        /// Gets a value indicating whether Saturation is supported.
+        /// </summary>
+        /// <value>
+        /// <c>true</c> if this instance is supported; otherwise, <c>false</c>.
+        /// </value>
+        public override bool IsSupported
+            => ApiInformation.IsApiContractPresent("Windows.Foundation.UniversalApiContract", 3);
+
+        /// <summary>
+        /// Gets the name of the effect.
+        /// </summary>
+        /// <value>
+        /// The name of the effect.
+        /// </value>
+        public override string EffectName { get; } = "Brightness";
+
+        /// <summary>
+        /// Applies the effect.
+        /// </summary>
+        /// <returns>
+        /// An array of strings of the effect properties to change.
+        /// </returns>
+        public override string[] ApplyEffect()
+        {
+            var saturationEffect = new SaturationEffect
+            {
+                Saturation = 1f,
+                Name = EffectName,
+                Source = new CompositionEffectSourceParameter("source")
+            };
+
+            var propertyToChange = $"{EffectName}.Brightness";
+            var propertiesToAnimate = new[] { propertyToChange };
+
+            EffectBrush = Compositor.CreateEffectFactory(saturationEffect, propertiesToAnimate).CreateBrush();
+            EffectBrush.SetSourceParameter("source", Compositor.CreateBackdropBrush());
+
+            return propertiesToAnimate;
+        }
+    }
+}
